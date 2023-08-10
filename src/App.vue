c5b51d6de041b6ce4c029ea237cfec38ffb85910
Semion Krapivin <homer1996@gmail.com>
2023-08-11 01:38:01 +0300

    feat: add post creation methods

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
+  <div class="app">
+    <form @submit.prevent>
+      <h3>Add new post</h3>
+      <input
+        v-bind:value="title"
+        @input="title = $event.target.value"
+        class="input"
+        type="text"
+        name=""
+        id=""
+        placeholder="Title"
+      />
+      <input
+        v-bind:value="body"
+        @input="body = $event.target.value"
+        class="input"
+        type="text"
+        name=""
+        id=""
+        placeholder="Description"
+      />
+      <button class="btn" @click="createPost">Create</button>
+    </form>
+
     <div class="post" v-for="post in posts" :key="post.id">
       <div class=""><strong>Title: </strong>{{ post.title }}</div>
       <div class=""><strong>Description: </strong>{{ post.body }}</div>
@@ -14,16 +37,21 @@ export default {
       posts: [
         { id: 1, title: 'JavaScript', body: 'Lorem ipsum 1' },
         { id: 2, title: 'JavaScript 2', body: 'Lorem ipsum 2' },
-        { id: 3, title: 'JavaScript 3', body: 'Lorem ipsum 3' }
-      ]
+        { id: 3, title: 'JavaScript 3', body: 'Lorem ipsum 3' },
+        { id: 4, title: 'JavaScript 4', body: 'Lorem ipsum 4' }
+      ],
+      title: '',
+      body: ''
     }
   },
   methods: {
-    addLike() {
-      this.likes += 1
-    },
-    addDislike() {
-      this.dislikes += 1
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+      }
+      this.posts.push(newPost)
     }
   }
 }
@@ -35,9 +63,41 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
+.app {
+  padding: 20px;
+}
 .post {
   padding: 10px;
-  border: 2px solid green;
+
+  border-radius: 10px;
   margin-top: 10px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+.btn {
+  margin-top: 10px;
+  align-self: flex-end;
+  padding: 10px;
+  background: none;
+  border: 2px solid turquoise;
+  cursor: pointer;
+  transition: 0.3s;
+  font-weight: 800;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
+.btn:hover {
+  border: 2px solid forestgreen;
+}
+.input {
+  width: 100%;
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 10px 10px;
+  margin-top: 10px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
 }
 </style>
