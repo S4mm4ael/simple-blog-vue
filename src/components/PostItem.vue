e66c8adefffb05ca53103084af10a13ce408c989
Semion Krapivin <homer1996@gmail.com>
2023-08-14 18:54:03 +0300

    refactor: decomposition for Items List

---
+++ src/components/PostItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="post">
+    <div class=""><strong>Title: </strong>{{ post.title }}</div>
+    <div class=""><strong>Description: </strong>{{ post.body }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post {
+  padding: 10px;
+  border-radius: 10px;
+  margin-top: 10px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
+</style>
