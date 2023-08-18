37600170f074e0e4471b2db4e5074463d773ae1b
Semion Krapivin <homer1996@gmail.com>
2023-08-18 13:58:59 +0300

    feat: add select UI component

---
+++ src/components/UI/SelectRegular.vue
@@ -0,0 +1,32 @@
+<template>
+  <select :modelValue="optionSelected" @change="changeOption" class="select">
+    <option disabled selected value="">Sort post by</option>
+    <option v-for="option in options" :key="option.value" :value="option.value">
+      {{ option.name }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'SelectRegular',
+  props: {
+    modelValue: { type: String },
+    options: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    changeOption(event) {
+      console.log(event)
+      this.$emit('update: optionSelected', event)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.select {
+}
+</style>
