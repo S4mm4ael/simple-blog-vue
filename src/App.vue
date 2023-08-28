5095ceac8f8d1024353d0a391b492c0492ec3e1e
SEMION KRAPIVIN <homer1996@gmail.com>
2023-08-29 02:47:18 +0300

    refactor: replace axios to use mocked data

--- src/App.vue
+++ src/App.vue
@@ -25,9 +25,52 @@ export default {
   },
   data() {
     return {
-      posts: [],
+      posts: [
+        {
+          userId: 1,
+          id: 8,
+          title: 'dolorem dolore est ipsam',
+          body: 'dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae'
+        },
+        {
+          userId: 1,
+          id: 9,
+          title: 'nesciunt iure omnis dolorem tempora et accusantium',
+          body: 'consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas'
+        },
+        {
+          userId: 1,
+          id: 10,
+          title: 'optio molestias id quia eum',
+          body: 'quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error'
+        },
+        {
+          userId: 2,
+          id: 11,
+          title: 'et ea vero quia laudantium autem',
+          body: 'delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi'
+        },
+        {
+          userId: 2,
+          id: 12,
+          title: 'in quibusdam tempore odit est dolorem',
+          body: 'itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio'
+        },
+        {
+          userId: 2,
+          id: 13,
+          title: 'dolorum ut in voluptas mollitia et saepe quo animi',
+          body: 'aut dicta possimus sint mollitia voluptas commodi quo doloremque\niste corrupti reiciendis voluptatem eius rerum\nsit cumque quod eligendi laborum minima\nperferendis recusandae assumenda consectetur porro architecto ipsum ipsam'
+        },
+        {
+          userId: 2,
+          id: 14,
+          title: 'voluptatem eligendi optio',
+          body: 'fuga et accusamus dolorum perferendis illo voluptas\nnon doloremque neque facere\nad qui dolorum molestiae beatae\nsed aut voluptas totam sit illum'
+        }
+      ],
       dialogVisible: false,
-      isPostsLoading: true,
+      isPostsLoading: false,
       selectedSort: '',
       sortOptions: [
         { value: 'title', name: 'By name' },
@@ -61,9 +104,9 @@ export default {
       }
     }
   },
-  mounted() {
-    this.fetchPosts()
-  },
+  // mounted() {
+  //   this.fetchPosts()
+  // },
   computed: {
     selectSort() {
       if (this.selectedSort === 'id') {
