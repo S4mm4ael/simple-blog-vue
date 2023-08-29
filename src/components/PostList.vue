e37a3da736f8c7d44230a8f0a708c9edf28a7ed7
SEMION KRAPIVIN <homer1996@gmail.com>
2023-08-29 03:01:28 +0300

    style: add transitiongroup for posts

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,7 +1,9 @@
 <template>
   <div v-show="posts.length > 0">
     <h2>Posts</h2>
-    <PostItem v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove', post)" />
+    <transition-group name="postList">
+      <PostItem v-for="post in posts" :post="post" :key="post.id" @remove="$emit('remove', post)" />
+    </transition-group>
   </div>
   <h2 class="postList__emptyHeader" v-show="posts.length == 0">There is no posts...</h2>
 </template>
@@ -22,5 +24,18 @@ export default {
 <style scoped>
 .postList__emptyHeader {
   color: gray;
+}
+.postList-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.postList-enter-active,
+.postList-leave-active {
+  transition: all 1s ease;
+}
+.postList-enter-from,
+.postList-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
